daeae16406d7363046ad78533778fb66c6353800
Michael Scales <mscales@mindwireless.com>
2018-02-08 18:13:17 -0600

    Add test for successful append to contacts.
    Adding flow logic. Plan for full values, iterate through contacts, and
    insert record at Name spot.

--- addRecord.sh
+++ addRecord.sh
@@ -2,20 +2,20 @@
 # addRecord.sh
 # Michael Scales 2/8/2018
 
-echo $1
-echo $2
-echo $3
-echo $4
+NAME=$1
+ADDRESS=$2
+PHONE=$3
+EMAIL=$4
 
-while read p; do
-  echo $p
-done < contacts.data
-newRec="$1:$2:$3:$4"
-echo $newRec
-# sed -i -e "\$a$newRec" contacts.data
+echo "$NAME:$ADDRESS:$PHONE:$EMAIL" >> contacts.data
 
-while read p; do
-   echo $p
-done < contacts.data
+if [ $? ]; then
+   while read p; do
+      echo $p
+   done < contacts.data
+   EXIT_CODE=true
+else
+   EXIT_CODE=false
+fi
 
-true
+$EXIT_CODE
