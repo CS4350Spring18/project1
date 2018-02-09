521563f26ff0185a78e43981e54111e4f957c37f
Michael Scales <mscales@mindwireless.com>
2018-02-08 18:00:05 -0600

    loop through contacts.data and format parameters

--- addRecord.sh
+++ addRecord.sh
@@ -7,4 +7,15 @@ echo $2
 echo $3
 echo $4
 
+while read p; do
+  echo $p
+done < contacts.data
+newRec="$1:$2:$3:$4"
+echo $newRec
+# sed -i -e "\$a$newRec" contacts.data
+
+while read p; do
+   echo $p
+done < contacts.data
+
 true
