53fb35d29ed5d094d0440041e28ea3e7343db78a
Michael Scales <mscales@mindwireless.com>
2018-02-13 23:00:10 -0600

    Updated addRecord to enforce uniqueness.
    Added testing for unique records on name and email. Also added testing
    to test that these parameters are being passed to the script. Modified
    variable names to prevent confusion between what is global and local.

--- addRecord.sh
+++ addRecord.sh
@@ -2,20 +2,39 @@
 # addRecord.sh
 # Michael Scales 2/8/2018
 
-NAME=$1
-ADDRESS=$2
-PHONE=$3
-EMAIL=$4
-
-echo "$NAME:$ADDRESS:$PHONE:$EMAIL" >> contacts.data
-
-if [ $? ]; then
-   while read p; do
-      echo $p
-   done < contacts.data
-   EXIT_CODE=true
+addRecord()
+{
+   name=$1
+   address=$2
+   phone=$3
+   email=$4
+   echo "$name:$address:$phone:$email" >> contacts.data
+   if [ $? ]; then
+      return 0
+   else
+      return 1
+   fi
+}
+# Check that required fields are not blank.
+if [ -z $1 ] || [ -z $4 ]; then
+   printf "%s\n" "" "Error: Name and email are required fields." "You provided:" "Name: $1" \
+                 "Email: $4" "Aborting add record." ""
+   success=false
 else
-   EXIT_CODE=false
+   # Enforce unique name and email for records.
+   if . findRecord.sh $1 || . findRecord.sh $4; then
+      printf "%s\n" "" "Duplicate record found:" "Name: $1" "Email: $4" \
+                    "Name and email are unique identifiers." "Aborting add record." ""
+      success=false
+   else
+      addRecord $1 $2 $3 $4
+      # Verify that our record was successfully added.
+      if [ $? ]; then
+         sucess=true
+      else
+         success=false
+      fi
+   fi
 fi
 
-$EXIT_CODE
+$success
