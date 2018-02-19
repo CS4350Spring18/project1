22667b90ed9939fb38d76113ae9d2b928e23476f
Sherman <stt19@txstate.edu>
2018-02-19 11:16:05 -0600

    removed commented code
    oops

--- addRecord.sh
+++ addRecord.sh
@@ -2,30 +2,6 @@
 # addRecord.sh
 # Michael Scales 2/8/2018
 
-# NAME=$1
-# ADDRESS=$2
-# PHONE=$3
-# EMAIL=$4
-
-# echo "$NAME:$ADDRESS:$PHONE:$EMAIL" >> contacts.data
-
-# if [ $? ]; then
-#    while read p; do
-#       echo $p
-#    done < contacts.data
-#    EXIT_CODE=true
-# else
-#    EXIT_CODE=false
-# fi
-
-# $EXIT_CODE
-
-################################
-
-#!/bin/bash
-# addRecord.sh
-# Michael Scales 2/8/2018
-
 # Split the input into each field for the record
 IFS=':' read -r -a array <<< "$1"
 
